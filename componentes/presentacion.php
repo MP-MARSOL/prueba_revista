8f0ca9430ef74e22f48b564328c136525122bcb6
MP-PROGRAMADOR <mpprogramacion22@gmail.com>
2023-09-28 14:25:34 +0100

    card agregados

---
+++ componentes/presentacion.php
@@ -0,0 +1,8 @@
+<div class="card bg-dark text-white">
+  <img src="./img//7.jpeg" class="card-img" alt="...">
+  <div class="card-img-overlay">
+    <h5 class="card-title">Ceremonia de apertura del Año judiacial 2023</h5>
+    <p class="card-text">El presidente del consejo superior del poder judicial y primer magistrado de la nacion, su Excelencia Obiang Nguema Mbasogo.</p>
+    <p class="card-text">Leer Más</p>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -7,12 +7,14 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="./css/estilos.css">
     <title></title>
   </head>
   <body>
     <?php  require_once "./componentes/head.php";?>
     <?php  require_once "./componentes/slider.php";?>
+    <?php  require_once "./componentes/cardNoticias.php";?>
+    <?php  require_once "./componentes/presentacion.php";?>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
