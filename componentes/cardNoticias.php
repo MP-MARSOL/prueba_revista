398fa2cb4751294c76da779ffcd7429ac2da68f6
MP-PROGRAMADOR <mpprogramacion22@gmail.com>
2023-09-28 14:40:20 +0100

    agregando contenido en los card

--- componentes/cardNoticias.php
+++ componentes/cardNoticias.php
@@ -4,7 +4,7 @@
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Leer Más</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
@@ -12,23 +12,23 @@
         <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Leer Más</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="./img/5.jpeg" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">El jefe de Estado ha mantenido una reunion ese martes</h5>
+            <p class="card-text">El Jefe de Estado y Gobierno, ha calificado como tarea dificial la administracion de justicia en nuestro pais</p>
+            <a href="#" class="btn btn-primary">Leer Más</a>
         </div>
     </div>
     <div class="card" style="width: 18rem;">
         <img src="./img/1.jpeg" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title">Juramento de cargo del nuevo presidente de la corte</h5>
+            <p class="card-text">Francisco Evuy Nguema, nuevo presidente de la Corte Suprema de Justicia, ha acudido en la mañana del miercoles....</p>
+            <a href="#" class="btn btn-primary">Leer Más</a>
         </div>
     </div>
     
--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
 
     <link rel="stylesheet" href="componentes/styles.css">
+    <link rel="stylesheet" href="./css/estilos.css">
     <title></title>
   </head>
   <body>
